d15473e57c2dd757a31771200f714749cb81cfaa
Ansley Ussery <ansley@fb.com>
2021-02-02 07:13:25 -0800

    Document FX debugging
    ghstack-source-id: 232c5fce5a973445d2177e7a16a112793929b817
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/51530

--- torch/fx/__init__.py
+++ torch/fx/__init__.py
@@ -83,3 +83,4 @@ from .graph import Graph
 from .node import Node, map_arg
 from .proxy import Proxy
 from .interpreter import Interpreter as Interpreter, Transformer as Transformer
+from .subgraph_rewriter import replace_pattern
