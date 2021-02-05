e510528ec29e8c5c38214f9f86d59075361b9ba3
ErjiaGuan <erjia@fb.com>
2021-02-05 13:45:15 -0500

    [DataLoader] Implement FilterIterDataPipe
    ghstack-source-id: d829570974e1b3ccb3d68c6448dad8800c278884
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/51783

--- torch/utils/data/datapipes/iter/sampler.py
+++ torch/utils/data/datapipes/iter/sampler.py
@@ -1,7 +1,34 @@
 from torch.utils.data import IterDataPipe, Sampler, SequentialSampler
-from typing import TypeVar, Type, Iterator, Sized
+from typing import Callable, TypeVar, Type, Iterator, Sized
+
+from .callable import CallableIterDataPipe
 
 T_co = TypeVar('T_co', covariant=True)
+
+
+class FilterIterDataPipe(CallableIterDataPipe[T_co]):
+    r""" :class:`FilterIterDataPipe`.
+
+    Iterable DataPipe to filter elements from datapipe according to filter_fn.
+    args:
+        datapipe: Iterable DataPipe being filterd
+        filter_fn: Customized function mapping an element to a boolean.
+    """
+    def __init__(self,
+                 datapipe: IterDataPipe[T_co],
+                 *args,
+                 filter_fn: Callable[..., bool],
+                 **kwargs,
+                 ) -> None:
+        super().__init__(datapipe, *args, fn=filter_fn, **kwargs)
+
+    def __iter__(self) -> Iterator[T_co]:
+        for data in self.datapipe:
+            if (self.fn(data, *self.args, **self.kwargs)):
+                yield data
+
+    def __len__(self):
+        raise(NotImplementedError)
 
 
 class SamplerIterDataPipe(IterDataPipe[T_co]):
